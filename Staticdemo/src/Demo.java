9c23d382c81de486330c438f9f204625eeaaf947
Nitin8222 <nitinpatil8055@gmail.com>
2019-06-23 20:47:50 +0530

    First Commit

---
+++ Staticdemo/src/Demo.java
@@ -0,0 +1,43 @@
+
+class Demo {
+	static String d = "vgvgv";
+	String db = "vvgvgv";
+	
+ public void test (){
+		// TODO Auto-generated method stub
+		 String abc = "vd";   
+	System.out.println(abc);
+		}
+ public int test (int a, int b){
+		// TODO Auto-generated method stub
+	 
+		 return (a+b);
+	
+		}
+ public int test (int a, int b, int c){
+		// TODO Auto-generated method stub
+	 
+		 return (a+b+c);
+	
+		}
+ 
+}
+class D {
+	static String df = "agc";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	String db = "vdgf";
+		//System.out.println(Demo.d);
+		//Demo d = new Demo();
+		//System.out.println(super.db);
+		
+		
+	
+	
+	
+
+	
+}
+}
